e3d4445adea05b15e91989c1c96981efd0c4d72d
PATEL96 <patel.raj.official.96@gmail.com>
2024-03-16 17:50:06 +0530

    client: added timeLineElements.

--- src/app/TimeLine/page.tsx
+++ src/app/TimeLine/page.tsx
@@ -3,14 +3,28 @@ import Navbar from "@/components/Navbar/Navbar"
 import './page.css'
 import { TestData } from "./Defaultelements"
 import { timeLineElement } from "@/Types"
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog"
+
 
 export function TimeLine() {
 
 	return (
 		<div className="flex items-center">
-			<div className="TimeLine text-center">
+			<div className="TimeLine">
 				{TestData.map(element => (
-					<div className="Element bg-slate-400 m-10">{element.Data}</div>
+					<>
+						<div className="Element m-5" key={element.id}>
+							<p>{element.Data}</p>
+						</div>
+						<hr></hr>
+					</>
 				))}
 			</div>
 			<Navbar />
