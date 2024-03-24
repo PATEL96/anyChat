d064fe9448057eb12eaea4ac77a7c23292958f28
PATEL96 <patel.raj.official.96@gmail.com>
2024-03-24 14:06:22 +0530

    cleint: Added test

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,6 +1,7 @@
 import Credentials from "next-auth/providers/credentials";
 import Google from "next-auth/providers/google";
 import Github from "next-auth/providers/github";
+import Discord from "next-auth/providers/discord";
 import type { NextAuthOptions } from "next-auth";
 
 export const auhOptions: NextAuthOptions = {
@@ -33,9 +34,13 @@ export const auhOptions: NextAuthOptions = {
 			clientId: process.env.GOOGLE_ID as string,
 			clientSecret: process.env.GOOGLE_SECRET as string,
 		}),
-		Github({
-			clientId: process.env.GITHUB_ID as string,
-			clientSecret: process.env.GITUB_SECRET as string
+		// Github({
+		// 	clientId: process.env.GITHUB_ID as string,
+		// 	clientSecret: process.env.GITUB_SECRET as string
+		// }),
+		Discord({
+			clientId: process.env.DISCORD_ID as string,
+			clientSecret: process.env.DISCORD_SECRET as string
 		})
 	],
 }
