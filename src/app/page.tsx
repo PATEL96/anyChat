94aca774024ac9ec56ff46ecb4529f68baf1bc8a
PATEL96 <patel.raj.official.96@gmail.com>
2024-04-14 11:59:16 +0530

    client: added autoUpdates for every 5 mnts.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { TimeLine } from "../components/TimeLine/TimeLine"
 export default function Home() {
   return (
     <div className="flex justify-center items-center">
+      <link rel="icon" href="/favicon.ico" />
       <TimeLine />
     </div>
   );
