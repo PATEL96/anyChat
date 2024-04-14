166d953476cbb06a5e03b5f1543ee5238e998026
PATEL96 <patel.raj.official.96@gmail.com>
2024-04-14 10:35:12 +0530

    client: added new Routes.

--- src/lib/mongo.ts
+++ src/lib/mongo.ts
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 export default async function connectMongoDB() {
 	try{
 		await mongoose.connect(process.env.MONGO_URI as string);
-		console.log("connected sucessfully")
 	} catch(error){
 		console.log(error);
 	}
